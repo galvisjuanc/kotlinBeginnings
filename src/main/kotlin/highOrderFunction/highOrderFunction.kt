e4ca091541c477d534d62dc980e808ae91c4d6da
Juan Galvis <galvisjuanc@gmail.com>
2023-06-05 08:41:23 -0500

    High Order Functions

---
+++ src/main/kotlin/highOrderFunction/highOrderFunction.kt
@@ -0,0 +1,22 @@
+package highOrderFunction
+
+fun main(args: Array<String>) {
+    val largoDelValorInicial = superFuncion(valorInicial = "Hola!", block = {valor ->
+        valor.length
+    })
+    println(largoDelValorInicial)
+
+    val lambda: () -> String = funcionInception("JuanCamilo")
+    val valorLambda : String = lambda()
+    println(valorLambda)
+}
+
+fun superFuncion(valorInicial : String, block : (String) -> Int) : Int {
+    return block(valorInicial)
+}
+
+fun funcionInception(nombre : String) : () -> String {
+    return {
+        "Hola desde la lambda $nombre"
+    }
+}
